b844a7644d8e4c5e3feb1c2c7c9884865fc83116
GA1 <nvkga1@gmail.com>
2016-12-01 20:21:20 +0100

    Add turtle only for cases the turtle is inside

--- src/main/scala/TurtlePathSolver.scala
+++ src/main/scala/TurtlePathSolver.scala
@@ -1,5 +1,17 @@
-
+import scala.collection.immutable.Queue
 
 class TurtlePathSolver {
 
+  def solution(a: Array[Int]): Int = {
+
+    def solution(i: Int, last4: List[Int]): Int = {
+      if (i == a.length) - 1
+      else if (last4.length < 4) solution(i + 1, last4 :+ a.head)
+      else if (a.head >= last4(2)) i
+      else solution(i + 1, last4.tail :+ a.head)
+    }
+    solution(0, Nil)
+  }
+
 }
+
--- src/test/scala/TurtlePathSolverTest.scala
+++ src/test/scala/TurtlePathSolverTest.scala
@@ -1,5 +1,19 @@
+import org.scalatest.FunSuite
 
+class TurtlePathSolverTest extends FunSuite {
 
-class TurtlePathSolverTest {
+  val solver = new TurtlePathSolver()
+
+  test("solve for 5, 4, 3, 100") {
+    assert(solver.solution(Array(5, 4, 3, 100)) === 4)
+  }
+
+//  test("solve for 5, 4, 3, 2") {
+//    assert(solver.solution(Array(5, 4, 3, 2)) === -1)
+//  }
+
+//  test("solve for 1, 3, 2,  5, 4, 4, 6, 3, 2") {
+//    assert(solver.solution(Array(1, 3, 2,  5, 4, 4, 6, 3, 2)) === 7)
+//  }
 
 }
