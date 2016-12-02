c0c0f7238ead403eff62d2d370b69e9cb63c113f
GA1 <nvkga1@gmail.com>
2016-12-02 13:10:50 +0100

    Add BunnyJumpSolver

---
+++ src/main/scala/BunnyJumpSolver.scala
@@ -0,0 +1,24 @@
+
+class BunnyJump {
+
+  def rotate(s: String): IndexedSeq[String] = {
+    val N = s.length
+    (1 to (N)).scanLeft(s)((x, y) =>x.substring(1, N) + x.charAt(0)).tail
+  }
+
+  private def printRotations(rotations: IndexedSeq[String]): Unit = println(rotations.mkString(" "))
+
+  def rotateAndPrint(s: String): Unit = printRotations(rotate(s))
+}
+
+
+object BunnyJump {
+
+  def main(args: Array[String]) {
+    val lines = for (line <- io.Source.stdin.getLines) yield (line)
+    val solver = new RotateStringSolver()
+    val ss = lines.toList.tail
+    ss foreach {solver.rotateAndPrint(_)}
+  }
+
+}
