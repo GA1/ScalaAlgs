fed3673f912e7475abadbe74b75fac326ed89010
GA1 <nvkga1@gmail.com>
2016-12-05 20:35:30 +0100

    Update bunny jump

--- src/main/scala/BunnyJumpSolver.scala
+++ src/main/scala/BunnyJumpSolver.scala
@@ -2,15 +2,15 @@
 class BunnyJumpSolver {
 
   def solve(jumps: Array[BigInt]): BigInt = {
-    return jumps.foldLeft(BigInt("1"))(lcm(_, _))
+    return jumps.foldLeft(BigInt("1"))(leastCommonMultiple(_, _))
   }
 
-  def gcd(a: BigInt, b: BigInt): BigInt = {
-    if (a == 0) b else gcd(b % a, a)
+  private def greatestCommonDivisor(a: BigInt, b: BigInt): BigInt = {
+    if (a == 0) b else greatestCommonDivisor(b % a, a)
   }
 
-  def lcm(a: BigInt, b: BigInt): BigInt = {
-    a * b / gcd(a, b)
+  private def leastCommonMultiple(a: BigInt, b: BigInt): BigInt = {
+    a * b / greatestCommonDivisor(a, b)
   }
 
 }
