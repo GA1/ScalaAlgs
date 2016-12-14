afdaeb48f6443ca03093706b91517d8217afb21c
GA1 <nvkga1@gmail.com>
2016-12-14 19:07:03 +0100

    Fix tests for primality solver

--- src/test/scala/PrimalitySolverTest.scala
+++ src/test/scala/PrimalitySolverTest.scala
@@ -1,4 +1,3 @@
-import CaptainPrimeSolver.{CENTRAL, DEAD, LEFT, RIGHT}
 import org.scalatest.FunSuite
 
 
@@ -7,39 +6,39 @@ class PrimalitySolverTest extends FunSuite {
   val sovler = new PrimalitySolver()
 
   test("solve for 1 ") {
-    assert(CaptainPrimeSolver.isPrime(1) === false)
+    assert(sovler.solve(1) === false)
   }
 
   test("solve for 2") {
-    assert(CaptainPrimeSolver.isPrime(2) === true)
+    assert(sovler.solve(2) === true)
   }
 
   test("solve for 3") {
-    assert(CaptainPrimeSolver.isPrime(3) === true)
+    assert(sovler.solve(3) === true)
   }
 
   test("solve for 4") {
-    assert(CaptainPrimeSolver.isPrime(2*2) === false)
+    assert(sovler.solve(2*2) === false)
   }
 
   test("solve for 5") {
-    assert(CaptainPrimeSolver.isPrime(5) === true)
+    assert(sovler.solve(5) === true)
   }
 
   test("solve for 6") {
-    assert(CaptainPrimeSolver.isPrime(2*3) === false)
+    assert(sovler.solve(2*3) === false)
   }
 
   test("solve for 13") {
-    assert(CaptainPrimeSolver.isPrime(13) === true)
+    assert(sovler.solve(13) === true)
   }
 
   test("solve for 31*39") {
-    assert(CaptainPrimeSolver.isPrime(31*39) === false)
+    assert(sovler.solve(31*39) === false)
   }
 
   test("solve for 961,748,941") {
-    assert(CaptainPrimeSolver.isPrime(961748941) === true)
+    assert(sovler.solve(961748941) === true)
   }
 
 }
