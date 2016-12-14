b24450fce964cef363d8465117bc0bb6d301147a
GA1 <nvkga1@gmail.com>
2016-12-14 18:48:59 +0100

    Add CaptainPrimeSolver

---
+++ src/main/scala/CaptainPrimeSolver.scala
@@ -0,0 +1,43 @@
+
+object CaptainPrimeSolver {
+
+
+  abstract class PersonFate
+  case object CENTRAL extends PersonFate
+  case object LEFT extends PersonFate
+  case object RIGHT extends PersonFate
+  case object DEAD extends PersonFate
+
+  def isPrime(n: Int): Boolean = n != 1 && ((2 to Math.sqrt(n).toInt).toStream forall (n % _ != 0))
+
+  def containsZero(n: Int): Boolean = !n.toString.toList.find(_ == '0').isEmpty
+
+  def getNumbersFromRight(number: Int): List[Int] = {
+    val numInString = number.toString
+    val length = numInString.length
+    (1 until length).foldLeft(List[Int]())((acc, i) => numInString.substring(0, length - i).toInt :: acc)
+  }
+
+  def getNumbersFromLeft(n: Int): List[Int] = {
+    val numInString = n.toString
+    val length = numInString.length
+    (1 until length).foldLeft(List[Int]())((acc, i) => numInString.substring(i).toInt :: acc)
+  }
+
+}
+
+class CaptainPrimeSolver {
+  import CaptainPrimeSolver._
+
+  def solve(n: Int): PersonFate = {
+    if (!isPrime(n) || containsZero(n)) DEAD
+    else {
+      (getNumbersFromLeft(n).forall(isPrime(_)), getNumbersFromRight(n).forall(isPrime(_))) match {
+        case (true, true) => CENTRAL
+        case (true, false) => LEFT
+        case (false, true) => RIGHT
+        case _ => DEAD
+      }
+    }
+  }
+}
