0d849cc64908a6b88276a82a44936472a62675e9
GA1 <nvkga1@gmail.com>
2016-12-15 17:38:43 +0100

    Use for comprehensions instead of loops increments memory usage

--- src/main/scala/FlippingMatrixSolver.scala
+++ src/main/scala/FlippingMatrixSolver.scala
@@ -1,13 +1,13 @@
 
 class FlippingMatrixSolver {
 
-  def solve(m: Array[Array[Int]], n: Int) = {
-    var sum = 0
+  def solve(m: Array[Array[Int]], n: Int): Int = {
     val bound = 2*n - 1
-    for (i <- 0 until n)
-      for (j <- 0 until n)
-        sum += List(m(i)(j), m(i)(bound - j), m(bound - i)(j), m(bound - i)(bound - j)).max
-    sum
+    (for (
+      i <- 0 until n;
+      j <- 0 until n
+    ) yield List(m(i)(j), m(i)(bound - j), m(bound - i)(j), m(bound - i)(bound - j)).max
+    ).sum
   }
 
 }
