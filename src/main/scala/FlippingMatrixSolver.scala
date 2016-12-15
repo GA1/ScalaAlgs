6620876a757dfe9852cfba154133bf31e5f01b91
GA1 <nvkga1@gmail.com>
2016-12-15 17:21:55 +0100

    User for instead of while

--- src/main/scala/FlippingMatrixSolver.scala
+++ src/main/scala/FlippingMatrixSolver.scala
@@ -3,16 +3,10 @@ class FlippingMatrixSolver {
 
   def solve(m: Array[Array[Int]], n: Int) = {
     var sum = 0
-    var i = 0
     val bound = 2*n - 1
-    while (i < n) {
-      var j = 0
-      while (j < n) {
-        sum += Math.max(Math.max(m(i)(j), m(i)(bound - j)), Math.max(m(bound - i)(j), m(bound - i)(bound - j)))
-        j += 1
-      }
-      i += 1
-    }
+    for (i <- 0 until n)
+      for (j <- 0 until n)
+        sum += List(m(i)(j), m(i)(bound - j), m(bound - i)(j), m(bound - i)(bound - j)).max
     sum
   }
 
