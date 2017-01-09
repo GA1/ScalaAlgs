0777d198af7b0864c62144312a7e69046b125114
GA1 <nvkga1@gmail.com>
2017-01-09 16:43:05 +0100

    Remove prints from PangramSolver

--- src/main/scala/PangramSolver.scala
+++ src/main/scala/PangramSolver.scala
@@ -9,7 +9,6 @@ class PangramSolver {
   import PangramSolver.alphabet
 
   def solve(s: String): Boolean = {
-    print(s filter (c => alphabet.contains(c)))
     (s map (_.toLower) filter (c => alphabet.contains(c)) distinct).length == 26
   }
 
