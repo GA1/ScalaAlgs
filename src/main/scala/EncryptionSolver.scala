0f43dc570935b4a3e1906d20b7fdc446a17edb43
GA1 <nvkga1@gmail.com>
2017-01-10 12:43:56 +0100

    Make C a val in EncryptionSolver

--- src/main/scala/EncryptionSolver.scala
+++ src/main/scala/EncryptionSolver.scala
@@ -5,10 +5,9 @@ class EncryptionSolver {
   def solve(s: String): String= {
     val onlyLetters = s.toLowerCase
 
-    var C = Math.floor(Math.sqrt(onlyLetters.length)).toInt
+    val C = Math.ceil(Math.sqrt(onlyLetters.length)).toInt
     var R = Math.floor(Math.sqrt(onlyLetters.length)).toInt
 
-    if (C*R < s.length) C = C + 1
     if (C*R < s.length) R = R + 1
 
     val result = new StringBuilder()
