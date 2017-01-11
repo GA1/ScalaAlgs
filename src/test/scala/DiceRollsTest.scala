eb174f4f02d523449a570ebf322f45e090cce6ac
GA1 <nvkga1@gmail.com>
2017-01-11 17:32:32 +0100

    Add some tests for DiceRolls

--- src/test/scala/DiceRollsTest.scala
+++ src/test/scala/DiceRollsTest.scala
@@ -6,6 +6,10 @@ class DiceRollsTest extends FunSuite {
 
   private val solver = new DiceRolls()
 
+  test("""solve for 1""") {
+    assert(solver.solve(1) === 1)
+  }
+
   test("""solve for 2""") {
     assert(solver.solve(2) === 2)
   }
@@ -17,6 +21,15 @@ class DiceRollsTest extends FunSuite {
   test("""solve for 4""") {
     assert(solver.solve(4) === 8)
   }
+
+  test("""solve for 5""") {
+    assert(solver.solve(5) === 16)
+  }
+
+  test("""solve for 6""") {
+    assert(solver.solve(6) === 32)
+  }
+
 
 
 
