e2e196751f07161e3789225bab589566b194636a
GA1 <nvkga1@gmail.com>
2017-01-11 10:41:04 +0100

    Add dicerolls

---
+++ src/main/scala/DiceRolls.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by kamil on 1/11/17.
+  */
+class DiceRolls {
+
+
+  def solve(n: Int): Int = {
+    def count(n: Int, acc: Int): Int = acc match {
+      case _ if acc == n => 1
+      case _ if acc > n => 0
+      case _ if acc < n => count(n, acc + 1) + count(n, acc + 2) + count(n, acc + 3) + count(n, acc + 4) + count(n, acc + 5) + count(n, acc + 6)
+    }
+    count(n, 0)
+  }
+
+}
