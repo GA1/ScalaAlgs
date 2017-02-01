773a786bd3c134bddefcebd8ac8f14acf8c24a26
GA1 <nvkga1@gmail.com>
2017-02-01 17:44:23 +0100

    Make two versions of TapeEquilibriumSolver

--- src/main/scala/TapeEquilibriumSolver.scala
+++ src/main/scala/TapeEquilibriumSolver.scala
@@ -1,15 +1,26 @@
 
 class TapeEquilibriumSolver {
 
-  def solve(a: Array[Int]): Int = {
+  def solve1(a: Array[Int]): Int = {
 
     val accLeftSums: Array[Int] = a.scanLeft(0)(_ + _)
     val accRightSums: Array[Int] = a.scanRight(0)(_ + _)
 
-    (0 to a.length).foldLeft(Int.MaxValue)((min, i) => {
+    (1 until a.length).foldLeft(Int.MaxValue)((min, i) => {
       val diff = Math.abs(accLeftSums(i) - accRightSums(i))
       if (diff < min) diff else min
     })
   }
+
+  def solve2(a: Array[Int]): Int = {
+
+    val total = a.sum
+    a.drop(1).foldLeft(Int.MaxValue, a(0))((acc, curr) => {
+      val newDiff = Math.abs(acc._2 - (total - acc._2))
+      (Math.min(acc._1,  newDiff), acc._2 + curr)
+    })._1
+
+  }
+
 
 }
--- src/test/scala/TapeEquilibriumSolverTest.scala
+++ src/test/scala/TapeEquilibriumSolverTest.scala
@@ -6,19 +6,33 @@ class TapeEquilibriumSolverTest extends FunSuite {
   private val solver = new TapeEquilibriumSolver()
 
   test("solve for 1, 2") {
-    assert(solver.solve(Array(1, 2)) === 1)
+    assert(solver.solve1(Array(1, 2)) === 1)
+    assert(solver.solve2(Array(1, 2)) === 1)
+  }
+
+  test("solve for 1, 1") {
+    assert(solver.solve1(Array(1, 1)) === 0)
+    assert(solver.solve2(Array(1, 1)) === 0)
+  }
+
+  test("solve for 1, -1") {
+    assert(solver.solve1(Array(1, -1)) === 2)
+    assert(solver.solve2(Array(1, -1)) === 2)
   }
 
   test("solve for 1, 1, 2") {
-    assert(solver.solve(Array(1, 1, 2)) === 0)
+    assert(solver.solve1(Array(1, 1, 2)) === 0)
+    assert(solver.solve2(Array(1, 1, 2)) === 0)
   }
 
   test("solve for 1, 2, 3, 6") {
-    assert(solver.solve(Array(1, 2, 3, 6)) === 0)
+    assert(solver.solve1(Array(1, 2, 3, 6)) === 0)
+    assert(solver.solve2(Array(1, 2, 3, 6)) === 0)
   }
 
   test("solve for 3, 1, 2, 4, 3") {
-    assert(solver.solve(Array(3, 1, 2, 4, 3)) === 1)
+    assert(solver.solve1(Array(3, 1, 2, 4, 3)) === 1)
+    assert(solver.solve2(Array(3, 1, 2, 4, 3)) === 1)
   }
 
 
