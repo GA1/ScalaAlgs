db62bd4ff4995cd1c0477982d227b9155906edf8
GA1 <nvkga1@gmail.com>
2017-02-01 16:49:29 +0100

    Finish Tape Equilibrium

--- src/test/scala/TapeEquilibriumSolverTest.scala
+++ src/test/scala/TapeEquilibriumSolverTest.scala
@@ -13,5 +13,14 @@ class TapeEquilibriumSolverTest extends FunSuite {
     assert(solver.solve(Array(1, 1, 2)) === 0)
   }
 
+  test("solve for 1, 2, 3, 6") {
+    assert(solver.solve(Array(1, 2, 3, 6)) === 0)
+  }
+
+  test("solve for 3, 1, 2, 4, 3") {
+    assert(solver.solve(Array(3, 1, 2, 4, 3)) === 1)
+  }
+
+
 
 }
