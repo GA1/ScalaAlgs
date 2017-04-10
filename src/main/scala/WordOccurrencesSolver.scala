5a72ff673f907a396c47ccb5c67e1fd075bfae3c
GA1 <nvkga1@gmail.com>
2017-04-10 15:35:46 +0200

    Improve time complexity of WordOccurrences by using sliding

--- src/main/scala/WordOccurrencesSolver.scala
+++ src/main/scala/WordOccurrencesSolver.scala
@@ -1,14 +1,7 @@
 
 class WordOccurrencesSolver {
 
-  def solve(word: String, text: String): Int = {
-    val n = word.length
-    def solve(acc: Int, word: String, sb: String): Int = sb match {
-      case _ if sb.length < n => acc
-      case _ if sb.substring(0, n) == word => solve(acc + 1, word, sb.tail)
-      case _ => solve(acc, word, sb.tail)
-    }
-    solve(0, word, text)
-  }
+  def solve(word: String, text: String): Int = text.sliding(word.length).count(window => window == word)
+
 
 }
