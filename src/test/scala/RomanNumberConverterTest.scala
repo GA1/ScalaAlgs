64a941bdcce7de1bcfa92bd2b69a194f41ced6b4
GA1 <nvkga1@gmail.com>
2017-04-14 15:09:59 +0200

    Add a lot of tests for RomanRonverter

--- src/test/scala/RomanNumberConverterTest.scala
+++ src/test/scala/RomanNumberConverterTest.scala
@@ -2,49 +2,56 @@ import org.scalatest.FunSuite
 
 class RomanNumberConverterTest extends FunSuite {
 
+  private val converter = new RomanNumberConverter()
+
   test("convert I") {
-    val converter = new RomanNumberConverter()
     assert(converter.convert("I") === 1)
   }
 
   test("convert II") {
-    val converter = new RomanNumberConverter()
     assert(converter.convert("II") === 2)
   }
 
   test("convert III") {
-    val converter = new RomanNumberConverter()
     assert(converter.convert("III") === 3)
   }
 
   test("convert V") {
-    val converter = new RomanNumberConverter()
     assert(converter.convert("V") === 5)
   }
 
   test("convert IV") {
-    val converter = new RomanNumberConverter()
     assert(converter.convert("IV") === 4)
   }
 
   test("convert XXXVIII") {
-    val converter = new RomanNumberConverter()
     assert(converter.convert("XXXVIII") === 38)
   }
 
   test("convert XXXIX") {
-    val converter = new RomanNumberConverter()
     assert(converter.convert("XXXIX") === 39)
   }
 
   test("convert XLI") {
-    val converter = new RomanNumberConverter()
     assert(converter.convert("XLI") === 41)
   }
 
   test("convert MMMCMXCIX") {
-    val converter = new RomanNumberConverter()
     assert(converter.convert("MMMCMXCIX") === 3999)
+  }
+
+  test("convert for final test") {
+    assert(converter.convert("XIX") === 19)
+    assert(converter.convert("XXXIII") === 33)
+    assert(converter.convert("CXLVII") === 147)
+    assert(converter.convert("CCCLXV") === 365)
+    assert(converter.convert("XCIV") === 94)
+    assert(converter.convert("CCLXXXIX") === 289)
+    assert(converter.convert("MCMXCVII") === 1997)
+    assert(converter.convert("DCCCXCVIII") === 898)
+    assert(converter.convert("DCCLXIX") === 769)
+    assert(converter.convert("MMMDXLVIII") === 3548)
+    assert(converter.convert("MMCMLXXI") === 2971)
   }
 
 }
